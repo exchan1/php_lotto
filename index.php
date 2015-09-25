30dc9423ba91dd6f273fb1102f76ca74ea641849
lotto <exchan1@gmail.com>
2015-09-25 14:44:52 +0900

    수정

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 include 'class.snoopy.php';
 header("Content-Type: text/html; charset=UTF-8");
-echo($_GET['kai'].'회차<br />');
 
 $snoopy = new snoopy;
 $getUrl = "http://www.645lotto.net/lotto645Confirm.do?method=byWin&drwNo=".$_GET['kai'];
@@ -18,13 +17,17 @@ for( $i=0 ; $i < sizeof($out[0]) ; $i++ ){
 }
 echo('</select>');
 
-echo('<p>');
-$pattern='/img src="\/img\/common\/ball_[0-9]*.png/';
-preg_match_all($pattern,$snoopy->results,$out);
-for($i=0;$i<=6;$i++){
-	$num[$i]=str_replace(".png","",str_replace('img src="/img/common/ball_',"",$out[0][$i]));
-	echo(($i==6) ? '보너스 : ' : '');
-	echo($num[$i].'<br />');
+echo(($_GET['kai']) ? '<br />'.$_GET['kai'].'회차<br />' : '');
+
+if($_GET['kai']){
+	echo('<p>');
+	$pattern='/img src="\/img\/common\/ball_[0-9]*.png/';
+	preg_match_all($pattern,$snoopy->results,$out);
+	for($i=0;$i<=6;$i++){
+		$num[$i]=str_replace(".png","",str_replace('img src="/img/common/ball_',"",$out[0][$i]));
+		echo(($i==6) ? '보너스 : ' : '');
+		echo($num[$i].'<br />');
+	}
+	echo('</p>');
 }
-echo('</p>');
 ?>
